b311ceb8a43b092e2e163b29e6b7f2fb37039b3f
Sebastian <31518719+sbra4670@users.noreply.github.com>
2018-05-25 13:49:32 -0600

    Experimenting with the Windows API using c++
    Made a project that, when ran, either signs the current user out or locks the windows computer in use.

---
+++ LogOff/LogOff.h
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+#include "Windows.h"
+#include <iostream>
+
+#ifndef LogOff_H
+#define LogOff_H
+
+using namespace std;
+
+class SignOut
+{
+public:
+	void Launch();
+	void test();
+};
+
+#endif
---
+++ LogOff/Main.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "LogOff.h"
+
+int main()
+{
+	SignOut app;
+
+	app.Launch();
+
+	cin.get();
+
+	return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int nCmdShow)
+{
+	return 0;
+}
